6481f3eb8d86ccd461d0949b8de3f92ce7341d35
Arihant Surana <ari.surana@gmail.com>
2019-02-18 00:31:39 +1100

    Added more docker permissions and minikube

--- ubuntu/sdk.sh
+++ ubuntu/sdk.sh
@@ -13,5 +13,17 @@ echo "install docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -fsSL https://get.docker.com -o get-docker.sh
 ./get-docker.sh
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo docker run hello-world
 
+echo "Install minikube"
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+sudo cp minikube /usr/local/bin && rm minikube
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 
